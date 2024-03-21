d4e5e32517f7e08a518962867593f1d6d6013de9
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-21 12:59:33 +0100

    Create Program file

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program {
+        static void Main(string[] args) {
+            CarSalesman steveTheSalesman = new CarSalesman("Steve", "Rogers");
+            Console.WriteLine(steveTheSalesman.FullName);
+            steveTheSalesman.Sell();
+
+            RetailSalesPerson erikTheSalesman = new RetailSalesPerson("Erik", "Erikson");
+            Console.WriteLine(erikTheSalesman.FullName);
+            erikTheSalesman.Sell();
+            erikTheSalesman.SellVirtual();
+
+        }
+    }
+    
+}
