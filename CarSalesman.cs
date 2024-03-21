356725a10978771edda020b7e975e89f75893bd3
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-21 12:57:11 +0100

    Create CarSalesman class extends Salesman class

---
+++ CarSalesman.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class CarSalesman : Salesman
+{
+    public CarSalesman(string firstName, string lastName) : base(firstName, lastName)
+    {
+    }
+
+    public override void Sell()
+    {
+        Console.WriteLine(string.Format("Hi my name is {0}. I would recommend you to buy this car!", this.FullName));
+    }
+}
