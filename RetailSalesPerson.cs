d79272b2817c16303c82b89a9765978b0b5e073c
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-21 12:56:44 +0100

    Create RetailSalesPerson class extends Salesman class

---
+++ RetailSalesPerson.cs
@@ -0,0 +1,12 @@
+using System;
+public class RetailSalesPerson : Salesman
+{
+    public RetailSalesPerson(string firstName, string lastName) : base(firstName, lastName)
+    {
+    }
+
+    public override void Sell()
+    {
+        Console.WriteLine(string.Format("Hi my name is {0}. I would recommend you to buy this pen!", this.FullName));
+    }
+}
