9caf22fc018c41acf69755adc71a3ba176659048
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-22 12:16:42 +0100

    Using pure if-else-statements

--- Developer.cs
+++ Developer.cs
@@ -7,7 +7,17 @@ public abstract class Developer {
         this._mainDevLanguage = mainDevLanguage;
     }
 
-    public virtual void Code() {
-        Console.WriteLine("I am coding  using my main dev language {0}", this._mainDevLanguage);
+    public virtual void Code(string firstExtraDevLang = null, string secondExtraDevLang = null) {
+         if (firstExtraDevLang == null && secondExtraDevLang == null) {
+            Console.WriteLine("I am coding using my main dev language {0}", this._mainDevLanguage);
+
+         } else if (firstExtraDevLang != null && secondExtraDevLang != null) {
+
+        Console.WriteLine("I am coding  using my main dev language {0}. Additionally, I am using {1} and {2} extra dev languages", this._mainDevLanguage, firstExtraDevLang, secondExtraDevLang);
+         } else if (firstExtraDevLang != null) {
+            Console.WriteLine(string.Format("I am coding using my main dev langauge {0}. Additionally, I am using {1} extra dev language.", this._mainDevLanguage, firstExtraDevLang));
+         } else if (secondExtraDevLang != null) {
+            Console.WriteLine(string.Format("I am coding using my main dev langauge {0}. Additionally, {1} extra dev language.", this._mainDevLanguage, secondExtraDevLang));
+         }
     }
 }
