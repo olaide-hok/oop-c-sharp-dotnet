523f7061fb8fd6268e07773bc7e6487a06155fbb
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-22 12:24:52 +0100

    Implement static polymorphism

--- Developer.cs
+++ Developer.cs
@@ -7,17 +7,16 @@ public abstract class Developer {
         this._mainDevLanguage = mainDevLanguage;
     }
 
-    public virtual void Code(string firstExtraDevLang = null, string secondExtraDevLang = null) {
-         if (firstExtraDevLang == null && secondExtraDevLang == null) {
-            Console.WriteLine("I am coding using my main dev language {0}", this._mainDevLanguage);
+    // Static Polymorphism
+     public virtual void Code() {
+         Console.WriteLine("I am coding using my main dev language {0}", this._mainDevLanguage);
+     }
 
-         } else if (firstExtraDevLang != null && secondExtraDevLang != null) {
+      public virtual void Code(string extraDevLang) {
+         Console.WriteLine(string.Format("I am coding using my main dev langauge {0}. Additionally, I am using {1} as extra dev language.", this._mainDevLanguage, extraDevLang));
+      }
 
-        Console.WriteLine("I am coding  using my main dev language {0}. Additionally, I am using {1} and {2} extra dev languages", this._mainDevLanguage, firstExtraDevLang, secondExtraDevLang);
-         } else if (firstExtraDevLang != null) {
-            Console.WriteLine(string.Format("I am coding using my main dev langauge {0}. Additionally, I am using {1} extra dev language.", this._mainDevLanguage, firstExtraDevLang));
-         } else if (secondExtraDevLang != null) {
-            Console.WriteLine(string.Format("I am coding using my main dev langauge {0}. Additionally, {1} extra dev language.", this._mainDevLanguage, secondExtraDevLang));
-         }
-    }
+       public virtual void Code(string firstExtraDevLang, string secondExtraDevLang) {
+         Console.WriteLine("I am coding  using my main dev language {0}. Additionally, I am using {1} and {2} as extra dev languages", this._mainDevLanguage, firstExtraDevLang, secondExtraDevLang);
+       }
 }
