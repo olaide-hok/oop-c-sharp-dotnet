7b6f5a23fa11c87ba57dab41f4c93538a574b898
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-22 18:46:13 +0100

    Create OnlineMarketer class

---
+++ OnlineMarketer.cs
@@ -0,0 +1,12 @@
+using System;
+public class OnlineMarketer : Salesman
+{
+    public OnlineMarketer(string firstName, string lastName) : base(firstName, lastName)
+    {
+    }
+
+    public override void Sell()
+    {
+        Console.WriteLine(string.Format("Hi my name is {0}. I would recommend you to buy this online product!", this.FullName));
+    }
+}
