5fea992ab4c842ead90d7bba1f260cb07b9ee361
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-22 22:02:06 +0100

    Implement Encapsulation
    Add definition for parent class protected access types

--- RetailSalesPerson.cs
+++ RetailSalesPerson.cs
@@ -5,12 +5,37 @@ public class RetailSalesPerson : Salesman, SelfDeveloping
     {
     }
 
-    public override void Sell()
-    {
-        Console.WriteLine(string.Format("Hi my name is {0}. I would recommend you to buy this pen!", this.FullName));
-    }
+    // public override void Sell()
+    // {
+    //     // Console.WriteLine(string.Format("Hi my name is {0}. I would recommend you to buy this pen!", this.FullName));
+    // }
 
     public void Develop() {
         Console.WriteLine("I develop myself by watching retail sales training videos.");
+    }
+
+    protected override void Engage()
+    {
+        Console.WriteLine("Hello");
+    }
+
+    protected override void EstablishTrust()
+    {
+        Console.WriteLine("I just want quickly help you.");
+    }
+
+    protected override void AddressCincerns()
+    {
+        Console.WriteLine("Everything is 80% discounted!");
+    }
+
+    protected override void DemoValue()
+    {
+        Console.WriteLine("Our customers love it");
+    }
+
+    protected override void Close()
+    {
+        Console.WriteLine("Buy this pen");
     }
 }
