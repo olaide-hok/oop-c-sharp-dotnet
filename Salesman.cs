1e0b9aee858097431822161202a2d31138c3db6e
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-03-22 22:00:55 +0100

    Implement Encapsulation
    Added protected access types
    
    Made Sell method a concrete method

--- Salesman.cs
+++ Salesman.cs
@@ -13,7 +13,21 @@ public abstract class Salesman {
         this._lastName = lastName;
     }
 
-    public abstract void Sell();
+    // Sell method made a concrete method 
+    public void Sell() {
+        this.Engage();
+        this.EstablishTrust();
+        this.AddressCincerns();
+        this.DemoValue();
+        this.Close();
+    }
+
+    protected abstract void Engage();
+    protected abstract void EstablishTrust();
+    protected abstract void AddressCincerns();
+    protected abstract void DemoValue();
+    protected abstract void Close();
+    
     public virtual void SellVirtual() {
          Console.WriteLine(string.Format("Hi my name is {0}. I would recommend you to buy this item!", this.FullName));
     }
